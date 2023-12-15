929417504045e2d2bcf12ba5beae1dadb8bcab2d
davidcoats <david@getsafetycone.com>
2023-12-15 11:06:12 -0800

    Initial commit.

---
+++ source/D8S.S0004/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.S0004
+{
+	/// <summary>
+	/// Personal Roslyn syntax element operation scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/D8S.S0004/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace D8S.S0004
+{
+    public static class Instances
+    {
+		
+    }
+}
